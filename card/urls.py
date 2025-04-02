63f4e08724b2c4504f6c9845f95a5b4deeb486c5
Виктор Бугай <ascon2913@gmail.com>
2025-04-02 15:38:28 +0500

    structure init

---
+++ card/urls.py
@@ -0,0 +1,7 @@
+from card.apps import CardConfig
+
+app_name = CardConfig.name
+
+urlpatterns = [
+
+]
---
+++ config/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("bonus_card.urls", namespace="bonus_card")),
+]
